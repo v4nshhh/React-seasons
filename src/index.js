77671eb37b6a194c1aeeec18a6943a822bcd80e4
fancehJS <vkapoor0112@gmail.com>
2021-07-07 00:07:31 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  state = { lat: null, long: null, errorMessage: "" };
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  componentDidMount = () => {
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+        this.setState({ long: position.coords.longitude });
+      },
+      (err) => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  };
+
+  renderContent = () => {
+    if (this.state.errorMessage && !this.state.lat && !this.state.long) {
+      return (
+        <div className="ui active dimmer">
+          <div className="ui big text loader">
+            <h1>{this.state.errorMessage}</h1>
+          </div>
+        </div>
+      );
+    }
+
+    if (!this.state.errorMessage && this.state.lat && this.state.long) {
+      return (
+        <div>
+          <SeasonDisplay lat={this.state.lat} long={this.state.long} />
+        </div>
+      );
+    }
+
+    return <Spinner message="Please allow location request" />;
+  };
+
+  render() {
+    return (
+      <div className="border red">
+        {this.renderContent()}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
